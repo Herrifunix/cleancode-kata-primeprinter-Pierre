3c301e3f24a7ddf2706e83d9287aa19e2689e82c
Pierre Zhou <sharkpi.zhou@gmail.com>
2024-12-12 11:47:51 +0100

    Activity 1

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -2,68 +2,83 @@ package com.cleancode.knuth;
 
 public class PrimePrinter {
     public static void main(String[] args) {
-		final int M = 1000;
+        final int M = 1000;
         final int RR = 50;
         final int CC = 4;
-        final int ORDMAX = 30;
-        int P[] = new int[M+1];
-        int PAGENUMBER;
-        int PAGEOFFSET;
-        int ROWOFFSET;
-        int C;
-        int J;
-        int K;
-        boolean JPRIME;
-        int ORD;
-        int SQUARE;
-        int N=0;
-        int MULT[] = new int[ORDMAX+1];
+        PrimePrinterHelper primePrinterHelper = new PrimePrinterHelper(M, RR, CC);
+        primePrinterHelper.generatePrimes();
+        primePrinterHelper.printPrimes();
+    }
+}
 
-        J=1;
-        K=1;
+class PrimePrinterHelper {
+    private final int M;
+    private final int RR;
+    private final int CC;
+    private final int ORDMAX = 30;
+    private final int[] P;
+    private final int[] MULT;
+
+    public PrimePrinterHelper(int M, int RR, int CC) {
+        this.M = M;
+        this.RR = RR;
+        this.CC = CC;
+        this.P = new int[M + 1];
+        this.MULT = new int[ORDMAX + 1];
+    }
+
+    public void generatePrimes() {
+        int J = 1;
+        int K = 1;
         P[1] = 2;
-        ORD = 2;
-        SQUARE = 9;
+        int ORD = 2;
+        int SQUARE = 9;
+        boolean JPRIME;
 
         while (K < M) {
             do {
                 J += 2;
-                if( J == SQUARE) {
+                if (J == SQUARE) {
                     ORD++;
-                    SQUARE=P[ORD]*P[ORD];
-                    MULT[ORD-1]=J;
+                    SQUARE = P[ORD] * P[ORD];
+                    MULT[ORD - 1] = J;
                 }
-                N=2;
-                JPRIME=true;
+                int N = 2;
+                JPRIME = true;
                 while (N < ORD && JPRIME) {
-                    while (MULT[N]<J)
+                    while (MULT[N] < J)
                         MULT[N] += P[N] + P[N];
                     if (MULT[N] == J)
-                        JPRIME=false;
+                        JPRIME = false;
                     N++;
                 }
             } while (!JPRIME);
             K++;
-            P[K]=J;
+            P[K] = J;
         }
-        PAGENUMBER = 1;
-        PAGEOFFSET = 1;
+    }
+
+    public void printPrimes() {
+        int PAGENUMBER = 1;
+        int PAGEOFFSET = 1;
+
         while (PAGEOFFSET <= M) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(M));
+            System.out.print(M);
             System.out.print(" Prime Numbers === Page ");
-            System.out.print(Integer.toString(PAGENUMBER));
+            System.out.print(PAGENUMBER);
             System.out.println("\n");
-            for (ROWOFFSET=PAGEOFFSET; ROWOFFSET <= PAGEOFFSET+RR-1; ROWOFFSET++) {
-                for (C = 0; C <= CC - 1; C++)
+
+            for (int ROWOFFSET = PAGEOFFSET; ROWOFFSET <= PAGEOFFSET + RR - 1 && ROWOFFSET <= M; ROWOFFSET++) {
+                for (int C = 0; C <= CC - 1; C++) {
                     if (ROWOFFSET + C * RR <= M)
                         System.out.printf("%10d", P[ROWOFFSET + C * RR]);
+                }
                 System.out.println();
             }
             System.out.println("\f");
             PAGENUMBER++;
-            PAGEOFFSET += RR*CC;
-
+            PAGEOFFSET += RR * CC;
         }
-	}
+    }
 }
