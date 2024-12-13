64190ce5fbdf6f2db355d5a39993434a9adf3757
Pierre Zhou <sharkpi.zhou@gmail.com>
2024-12-13 10:13:50 +0100

    Ajout de fonctions

--- build-url.js
+++ build-url.js
@@ -1,27 +1,44 @@
 function buildUrl(url, options) {
-    var queryString = [];
-    var key;
-    var builtUrl;
-
-    if (!url){
-    url = ''}
-
-    if (options.path) {
-            builtUrl = `${builtUrl}/${options.path}`;
-        }
-      if (options.queryParams) {
-        for (key in options.queryParams) {
-          if (options.queryParams.hasOwnProperty(key)) {
-            queryString.push(key + '=' + options.queryParams[key]);
-          }
-        }
-        builtUrl += '?' + queryString.join('&');
-      }
-
-      if (options.hash) {
-              builtUrl = `${builtUrl}#${options.hash}`;
-          }
+    if (!url) {
+        url = '';
     }
 
+    let builtUrl = url;
+    if (options) {
+        builtUrl = appendPath(builtUrl, options.path);
+        builtUrl = appendQueryParams(builtUrl, options.queryParams);
+        builtUrl = appendHash(builtUrl, options.hash);
+    }
     return builtUrl;
-};
+}
+
+function appendPath(url, path) {
+    if (path) {
+        return `${url}/${path}`;
+    }
+    return url;
+}
+
+function appendQueryParams(url, queryParams) {
+    if (!queryParams) {
+        return url;
+    }
+    const queryString = [];
+    for (const key in queryParams) {
+        if (queryParams.hasOwnProperty(key)) {
+            queryString.push(`${key}=${queryParams[key]}`);
+        }
+    }
+
+    if (queryString.length > 0) {
+        return `${url}?${queryString.join('&')}`;
+    }
+    return url;
+}
+
+function appendHash(url, hash) {
+    if (hash) {
+        return `${url}#${hash}`;
+    }
+    return url;
+}
